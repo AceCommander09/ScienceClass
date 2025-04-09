51d47fd3e86910252c23e8cc95caead95e836efd
AceCommander09 <138333283+AceCommander09@users.noreply.github.com>
2025-04-09 10:09:14 +1000

    Update games.js

--- public/assets/data/games.js
+++ public/assets/data/games.js
@@ -11,6 +11,11 @@ var games = [
     image: 'https://play-lh.googleusercontent.com/e5jzYTn7Mjh9Ghn-DFpVedkOlRfSOyCS2AFgXdTrADIuuUbjx6vP6r_YOM2j5m7uLQ'
   },
   {
+    id: 'Escape Road',
+    title: 'Escape Road',
+    url: 'https://escaperoad.io',
+  },
+  {
     id: 'eggycar',
     title: 'Eggy Car',
     url: 'https://assets.3kh0.net/eggycar/index.html',
